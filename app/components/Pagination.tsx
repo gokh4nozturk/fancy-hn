6a2a6f19a0a611541704a87266dd68a4716a43ff
gokhanozturk <gokhaanozturk@gmail.com>
2025-01-31 15:48:14 +0300

    feat: Add support for top and best Hacker News stories

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import {
 	ChevronFirst,
 	ChevronLast,
 	ChevronLeft,
 	ChevronRight,
 } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
 	currentPage: number;
@@ -15,9 +15,12 @@ interface Props {
 
 export default function Pagination({ currentPage, totalPages }: Props) {
 	const router = useRouter();
+	const searchParams = useSearchParams();
 
 	const handlePageChange = (page: number) => {
-		router.push(`/?page=${page}`);
+		const params = new URLSearchParams(searchParams.toString());
+		params.set("page", page.toString());
+		router.push(`/?${params.toString()}`);
 	};
 
 	return (
